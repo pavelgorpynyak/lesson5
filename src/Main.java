73cb34b088df50f5516747343dd5d6a9f75b9ec4
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-06-15 18:36:28 +0300

    lesson 5 finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       int[] weights = new int[]{90, 91, 93, 92, 85, 87, 89, 0, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        System.out.println(weights[1]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+
+        }
     }
 }
